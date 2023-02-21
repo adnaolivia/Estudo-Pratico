7204b14bd5ee52ccdbeb9e9c821150d89c201eef
adna olívia <88201392+adnaolivia@users.noreply.github.com>
2023-02-20 23:28:28 -0300

    curso Python 3 da Udemy
    iniciei dia 20/02/23

---
+++ aula01.py
@@ -0,0 +1,13 @@
+print("comentário é com #") #olha que legal :)
+print("print é uma função, por isso tem ()")
+print("ja sei disso, mas tô fazendo pra desencargo de consciência") #comentário
+#iggy ta estudando comigo
+print("hoje é 20/02/23")
+
+"""
+Docstring como comentário
+pode pular linha
+não é um comentário, o interpretador lê isso
+escreva suas notas, ou explique seu código
+não apareceu no terminal, hmmmm interessante
+"""
---
+++ print.py
@@ -0,0 +1,16 @@
+print(12, 34, sep=",", end="\n")
+print(56, 78, sep="-")
+print(9, 9, sep="/")
+print(56, 53, sep=" pode ser qualquer coisa ")
+
+
+"""
+print é uma função que recebe um argumento
+os argumentos ficam entre parênteses
+12, 34 - exemplos
+
+sep: argumento nomeado
+argumento não nomeado: a vírgula que não apareceu no terminal, antes de adicionar o sep
+
+\n - quebra de linha
+"""
