4213dbacb449900256e9305117a6d724f746bf97
adna olívia <88201392+adnaolivia@users.noreply.github.com>
2023-02-22 20:00:56 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,19 @@
+print("-------1º exercício-------")
+nome = "Adna"
+sobrenome = "Silva"
+idade = 20
+ano_nascimento = 2023 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.65
+
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Ano de nascimento:", ano_nascimento)
+print("É maior de idade?", maior_de_idade)
+print("Altura:", altura_metros)
+print("--------------")
+
+"""
+criar as variáveis que estão na saída
+"""
