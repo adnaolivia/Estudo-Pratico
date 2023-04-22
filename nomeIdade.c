8d0b6ade3cb7b19e49cfa50f86a4c4fa4e2dd36c
adna olívia <88201392+adnaolivia@users.noreply.github.com>
2023-04-21 21:57:09 -0300

    tirar dúvida!!!

---
+++ nomeIdade.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char n;
+    int i;
+    scanf("%s", &n);
+    scanf("%d", &i);
+    printf("nome: %s\n", n);
+    printf("idade: %d\n", i);
+    return 0;
+}
